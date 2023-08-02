91a7174bba4cb4bca645ce6beb7875af08f9e341
IvanSlivenko <ivan8822@ulr.net>
2023-08-02 14:44:00 +0300

    02-08-2023-14-43

---
+++ project/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+// // Отримуємо в константу елемент для виводу 
+// const rootElement = document.getElementById('root');
+
+// // Виводимо (відмальовуємо) нашу розмітку
+// ReactDOM.createRoot(rootElement).render(
+
+//     <h1>Мій перший React застосунок !!</h1>
+
+    
+// );
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const element = <h1>React independet study</h1>;
+
+root.render(element);
+
+
